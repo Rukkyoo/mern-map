1a3a822b332559b7aa14e4ad990cce2f6449e5a5
Michael Omonedo <rukkyomonedo@gmail.com>
2025-03-10 10:50:13 +0100

    working on route computation

--- mern-map/backend/app.js
+++ mern-map/backend/app.js
@@ -11,12 +11,48 @@ app.use(express.json());
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
-app.get("/api", (req, res) => {
-  res.send("Maps API");
+// Route computation endpoint
+app.get("/api/autocomplete", async (req, res) => {
+  const { input } = req.query; // Getting the input from the query string
+  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${GOOGLE_API_KEY}`;
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching autocomplete suggestions" });
+    console.log(error);
+  }
+});
+
+// Route computation endpoint
+app.get("/api/route", async (req, res) => {
+  const { origin, destination } = req.query;
+  const url = `https://routes.googleapis.com/directions/v2:computeRoutes?key=${GOOGLE_API_KEY}`;
+
+  const requestBody = {
+    origin: {
+      address: origin, // where the user is coming from
+    },
+    destination: {
+      address: destination, // where the user is going to
+    },
+    travelMode: "DRIVING", // the mode of travel
+  };
+
+  try {
+    const response = await axios.post(url, requestBody, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error computing route" });
+    console.log(error);
+  }
 });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
