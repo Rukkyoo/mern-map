f0cd5d41ac6f9faf7573739cd236eda128386c09
Michael Omonedo <rukkyomonedo@gmail.com>
2025-03-11 10:27:29 +0100

    completed frontend auto complete feature

--- mern-map/src/App.jsx
+++ mern-map/src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css";
+import React, { useState } from "react";
 import axios from "axios";
+import axiosInstance from "./axiosInstance";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [route, setRoute] = useState(null);
+
+  // Fetching the autocomplete suggestions
+  const fetchSuggestions = async (input) => {
+    try {
+      const response = await axiosInstance.get(`?input=${input}`);
+      setSuggestions(response.data.predictions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+// Handles the change event for the input field
+  const handleAutoCompleteChange = (e) => {
+    setInput(e.target.value);
+    fetchSuggestions(e.target.value);
+  };
+
+
+  // Handles the click event for the suggestion
+  const handleSuggestionClick = (suggestion) => {
+    setInput(suggestion.description);
+    setSuggestions([]);
+  };
+
   return (
     <div className="bg-black h-screen flex justify-center">
       <form className="my-10">
@@ -10,9 +41,22 @@ function App() {
         </label>
         <input
           type="search"
-          class="py-1 pl-10 text-sm text-gray-700 bg-gray-200 border border-white rounded-sm focus:outline-none focus:ring-2 focus:ring-gray-600"
+          onChange={handleAutoCompleteChange}
+          value={input}
+          class="p-1 mb-2 w-80 text-sm text-gray-700 bg-gray-200 border border-white rounded-sm focus:outline-none focus:ring-2 focus:ring-gray-600"
           placeholder="Search..."
         ></input>
+        <ul>
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion.id}
+              onClick={() => handleSuggestionClick(suggestion)}
+              className="cursor-pointer w-80 bg-slate-200 hover:bg-gray-300"
+            >
+              {suggestion.description}
+            </li>
+          ))}
+        </ul>
       </form>
     </div>
   );
