31669e12c18672b5fc437e1e080a9c5f7baaa2fc
Michael Omonedo <rukkyomonedo@gmail.com>
2025-03-11 12:31:36 +0100

    css changes

--- mern-map/src/App.jsx
+++ mern-map/src/App.jsx
@@ -46,29 +46,51 @@ function App() {
   return (
     <div className="bg-black h-screen flex justify-center">
       <form className="my-10 flex flex-col items-center">
-        <div>
-          <label for="search">
-            <span className="mr-2 text-white">Check Map: </span>{" "}
-          </label>
-          <input
-            type="search"
-            onChange={handleAutoCompleteChange}
-            value={input}
-            class="p-1 mb-2 w-80 text-sm text-gray-700 bg-gray-200 border border-white rounded-sm focus:outline-none focus:ring-2 focus:ring-gray-600"
-            placeholder="Search..."
-          ></input>
-        </div>
+        <label className="mb-3" for="search">
+          <span className="mb-10 text-white">Check Map: </span>{" "}
+        </label>
+        <input
+          type="search"
+          onChange={handleAutoCompleteChange}
+          value={input}
+          class="p-1 mb-2 w-80 text-sm text-gray-700 bg-gray-200 border border-white rounded-sm focus:outline-none focus:ring-2 focus:ring-gray-600"
+          placeholder="Search..."
+        ></input>
+
         <ul>
           {suggestions.map((suggestion) => (
             <li
               key={suggestion.placePrediction.place_id}
               onClick={() => handleSuggestionClick(suggestion)}
-              className="cursor-pointer w-80 bg-slate-200 hover:bg-gray-300"
+              className="cursor-pointer  w-80 bg-slate-200 hover:bg-gray-300"
             >
               {suggestion.placePrediction.text.text}
             </li>
           ))}
         </ul>
+        <div className="flex flex-row mt-8 justify-between px-2 items-center w-screen">
+          {" "}
+          <div>
+            <label className="mb-1 mt-6" for="origin">
+              <span className="mb-10 text-white">Origin: </span>{" "}
+            </label>
+            <input
+              className="p-1 mb-2 w-55 text-sm text-gray-700 bg-gray-200 border border-white rounded-sm focus:outline-none focus:ring-2 focus:ring-gray-600"
+              type="search"
+              placeholder="Where are you coming from?"
+            ></input>
+          </div>
+          <div>
+            <label className="mb-1 mt-6" for="origin">
+              <span className="mb-10 text-white">Destination: </span>{" "}
+            </label>
+            <input
+              className="p-1 mb-2 w-55 text-sm text-gray-700 bg-gray-200 border border-white rounded-sm focus:outline-none focus:ring-2 focus:ring-gray-600"
+              type="search"
+              placeholder="Where are you going to?"
+            ></input>
+          </div>
+        </div>
       </form>
     </div>
   );
