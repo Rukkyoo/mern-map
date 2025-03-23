0c0690ee8f862e4a33b73f2a108ed89ef444b0c2
Michael Omonedo <rukkyomonedo@gmail.com>
2025-03-23 14:36:16 +0100

    Uploaded backend to render and updated axiosinstance

--- mern-map/src/axiosInstance.jsx
+++ mern-map/src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api", // The base URL for the API
+    baseURL: "https://mern-map.onrender.com/api", // The base URL for the API
     headers: {
         "Content-Type": "application/json",
     },
